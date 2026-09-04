0d6183db3707ac7db1987b99c3d8df1d8cba6186
iucidiumphosphorus-coder <iucidium.phosphorus@gmail.com>
2026-09-04 13:53:39 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,22 @@
-# 1. S3バケット本体の作成
+# for_each を使ってプレフィックス付きバケットを一括管理
 resource "aws_s3_bucket" "app_buckets" {
   for_each = var.bucket_name_suffixes
   bucket   = "corporate-projectTest-${each.value}"
 }
 
-# 2. バージョニングの有効化
-resource "aws_s3_bucket_versioning" "app_buckets_versioning" {
+# バージョニングの有効化
+resource "aws_s3_bucket_versioning" "app_bucket_versioning" {
   for_each = aws_s3_bucket.app_buckets
   bucket   = each.value.id
-
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# 3. パブリックアクセスの完全ブロック
-resource "aws_s3_bucket_public_access_block" "app_buckets_block" {
+# パブリックアクセスの完全ブロック
+resource "aws_s3_bucket_public_access_block" "app_bucket_public_access" {
   for_each = aws_s3_bucket.app_buckets
   bucket   = each.value.id
-
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
