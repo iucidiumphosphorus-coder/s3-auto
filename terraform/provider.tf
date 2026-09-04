58caf29e477b7059d51e8c2e385f3611bec1d7d9
iucidiumphosphorus-coder <iucidium.phosphorus@gmail.com>
2026-09-04 10:46:14 +0900

    Update S3 bucket name in provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "corporate-s3-terraform-state-950177941522"
+    key            = "s3-auto/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "corporate-s3-terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
